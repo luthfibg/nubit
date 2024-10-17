8088cc528ac65832543a378ddd8289bb5568d1e4
Muhamad Luthfi Bangun Permadi <luthfinewzoneonline@gmail.com>
2024-10-17 22:07:34 +0700

    #8 perform catch-all errors and 404 error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -23,10 +23,16 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
+    try {
+      await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+      `;
+    } catch (error) {
+      return {
+        message: 'Database Error: Failed to Create Invoice.',
+      }
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -45,12 +51,18 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
  
   const amountInCents = amount * 100;
- 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Update Invoice.',
+    }
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -58,6 +70,14 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 // To Delete Invoice
 export async function deleteInvoice(id: string) {
+  // throw new Error('Not Implemented');
+  try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+    return { message: 'Invoice Deleted.' }
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Delete Invoice.',
+    }
+  }
 }
