ddf8d40513cd73d57c09249a77753a6879040eed
Muhamad Luthfi Bangun Permadi <luthfinewzoneonline@gmail.com>
2024-10-22 14:48:23 +0700

    #18.3 renaming create folder in customers to new

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -17,7 +17,10 @@ const links = [
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Customers',
+    href: '/dashboard/customers',
+    icon: UserGroupIcon
+  },
 ];
 
 export default function NavLinks() {
