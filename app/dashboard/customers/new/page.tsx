eb62f1609cc72ee9c0e2e0a8826fdfec2059d4a7
Muhamad Luthfi Bangun Permadi <luthfinewzoneonline@gmail.com>
2024-10-22 14:52:42 +0700

    #18.4 re-linking proper import components to new customer page

--- app/dashboard/customers/new/page.tsx
+++ app/dashboard/customers/new/page.tsx
@@ -1,5 +1,5 @@
-import Form from '@/app/ui/invoices/create-form';
-import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import Form from '@/app/ui/customers/create-form';
+import Breadcrumbs from '@/app/ui/customers/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
  
 export default async function Page() {
@@ -17,7 +17,7 @@ export default async function Page() {
           },
         ]}
       />
-      <Form customers={customers} />
+      <Form />
     </main>
   );
 }
